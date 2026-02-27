5da145be40897eb424de6c7fef78677e28b9ed8d
hs5385 <hs5385@srmist.edu.in>
2026-02-27 15:33:25 +0530

    UC2 Completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System Initialized Successfully");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a text: ");
+        String original = sc.next();
+        String reverse = "";
+        int l  = original.length();;
+        for (int i = l - 1; i >= 0; i--) {
+            reverse += original.charAt(i);
+        }
+        if (original.equalsIgnoreCase(reverse)) {
+            System.out.println("It is a palindrome!");
+        } else {
+            System.out.println("Not a palindrome.");
+        }
     }
 }
