1a59f9ff20d348cd11b6dede4b91263f1b793bf8
hs5385 <hs5385@srmist.edu.in>
2026-02-27 16:42:56 +0530

    UC5 completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,16 +1,22 @@
-
+import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "racecar";
-        char[] charArray = input.toCharArray();
-        boolean isMatch = compareEdges(charArray);
-        System.out.println("Do the start and end characters match? " + isMatch);
+        String input = "madam";
+        boolean result = isPalindrome(input);
+        System.out.println("Is '" + input + "' a palindrome? " + result);
     }
-    public static boolean compareEdges(char[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        if (arr.length == 0) return false;
-        System.out.println("Comparing: " + arr[start] + " and " + arr[end]);
-        return arr[start] == arr[end];
+    public static boolean isPalindrome(String text) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < text.length(); i++) {
+            stack.push(text.charAt(i));
+        }
+        for (int i = 0; i < text.length(); i++) {
+            char originalChar = text.charAt(i);
+            char reversedChar = stack.pop();
+            if (originalChar != reversedChar) {
+                return false;
+            }
+        }
+        return true;
     }
 }
