435cd1795d5530ee6dc0cdb4138020c4d58c298b
hs5385 <hs5385@srmist.edu.in>
2026-03-16 14:51:52 +0530

    UC9 Completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,59 +1,23 @@
-class Node {
-    char data;
-    Node next;
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
 public class PalindromeCheckerApp {
     public boolean isPalindrome(String str) {
-        if (str == null || str.isEmpty()) return true;
-        Node head = convertToLinkedList(str);
-        Node slow = head;
-        Node fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (str == null) {
+            return false;
         }
-        Node secondHalf = reverseList(slow);
-        Node firstHalf = head;
-        Node temp = secondHalf;
-        boolean result = true;
-        while (temp != null) {
-            if (firstHalf.data != temp.data) {
-                result = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            temp = temp.next;
-        }
-        reverseList(secondHalf);
-        return result;
+        return checkRecursive(str, 0, str.length() - 1);
     }
-    private Node convertToLinkedList(String str) {
-        Node head = new Node(str.charAt(0));
-        Node current = head;
-        for (int i = 1; i < str.length(); i++) {
-            current.next = new Node(str.charAt(i));
-            current = current.next;
+    private boolean checkRecursive(String str, int start, int end) {
+        if (start >= end) {
+            return true;
         }
-        return head;
-    }
-    private Node reverseList(Node head) {
-        Node prev = null;
-        Node current = head;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-        return prev;
+        return checkRecursive(str, start + 1, end - 1);
     }
     public static void main(String[] args) {
         PalindromeCheckerApp checker = new PalindromeCheckerApp();
-        String input = "racecar";
-        System.out.println("Is \"" + input + "\" a palindrome? " + checker.isPalindrome(input));
+        String word = "racecar";
+        boolean result = checker.isPalindrome(word);
+        System.out.println("Is '" + word + "' a palindrome? " + result);
     }
 }
